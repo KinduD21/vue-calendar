8145a75c59dabbe89ed6ccb8908dbf3aa0a2c99b
Vitalii Dudnik <dudnikvtl@gmail.com>
2025-09-15 23:43:56 +0300

    Updates

--- assets/routes-BfxdwTL2.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Page-cjK-AJT9.js","assets/index-y1OgO0KJ.js","assets/index-CPGz4w-K.css"])))=>i.map(i=>d[i]);
-import{_ as e}from"./index-y1OgO0KJ.js";const o=[{path:"vue-calendar",name:"CalendarPage",component:()=>e(()=>import("./Page-cjK-AJT9.js"),__vite__mapDeps([0,1,2]))}];export{o as default};
