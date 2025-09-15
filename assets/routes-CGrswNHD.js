3a914aaa3a3e1043be89555346364c284d820f8f
Vitalii Dudnik <dudnikvtl@gmail.com>
2025-09-15 22:16:49 +0300

    Updates

---
+++ assets/routes-CGrswNHD.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Page-2bldemGl.js","assets/index-Ci0aUN_F.js","assets/index-CsXscAPg.css"])))=>i.map(i=>d[i]);
+import{_ as a}from"./index-Ci0aUN_F.js";const o=[{path:"calendar",name:"CalendarPage",component:()=>a(()=>import("./Page-2bldemGl.js"),__vite__mapDeps([0,1,2]))}];export{o as default};
